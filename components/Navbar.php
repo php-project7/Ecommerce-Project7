736a735858b804b12072cbad87c8613bea9aad09
DeekCS <31720001079@std.bau.edu.jo>
2022-01-29 20:53:20 +0200

    handle myacount

--- components/Navbar.php
+++ components/Navbar.php
@@ -382,24 +382,3 @@
             </div><!-- End .modal-content -->
         </div><!-- End .modal-dialog -->
     </div>
-
-
-
-
-        <script src="../assets/js/jquery.min.js"></script>
-    <script src="../assets/js/bootstrap.bundle.min.js"></script>
-    <script src="../assets/js/jquery.hoverIntent.min.js"></script>
-    <script src="../assets/js/jquery.waypoints.min.js"></script>
-    <script src="../assets/js/superfish.min.js"></script>
-    <script src="../assets/js/owl.carousel.min.js"></script>
-    <script src="../assets/js/wNumb.js"></script>
-    <script src="../assets/js/bootstrap-input-spinner.js"></script>
-    <script src="../assets/js/jquery.magnific-popup.min.js"></script>
-    <script src="../assets/js/nouislider.min.js"></script>
-    <!-- Main JS File -->
-    <script src="../assets/js/main.js"></script>
-</body>
-
-
-<!-- molla/category-list.html  22 Nov 2019 10:02:52 GMT -->
-</html>
