55ba35918e7bd148a8e0a18dd6115e8d3202aa76
DeekCS <31720001079@std.bau.edu.jo>
2022-01-30 10:36:33 +0200

    update

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -87,8 +87,8 @@ if(isset($_POST['submit_save'])){
             <nav aria-label="breadcrumb" class="breadcrumb-nav mb-3">
                 <div class="container">
                     <ol class="breadcrumb">
-                        <li class="breadcrumb-item"><a href="index.html">Home</a></li>
-                        <li class="breadcrumb-item"><a href="#">Shop</a></li>
+                        <li class="breadcrumb-item"><a href="../../index.php">Home</a></li>
+                        <li class="breadcrumb-item"><a href="./category-list.php">Shop</a></li>
                         <li class="breadcrumb-item active" aria-current="page">My Account</li>
                     </ol>
                 </div><!-- End .container -->
