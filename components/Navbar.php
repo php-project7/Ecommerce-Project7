399b689f29610b88f772a205dd023039c9c9d2cc
Mahdi-Suleiman <mahdi.suleimann@gmail.com>
2022-01-31 16:05:58 +0200

    backup

--- components/Navbar.php
+++ components/Navbar.php
@@ -37,7 +37,7 @@ if (isset($_GET['logout'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Molla - Bootstrap eCommerce Template</title>
+    <title>Molla eCommerce </title>
     <meta name="keywords" content="HTML5 Template">
     <meta name="description" content="Molla - Bootstrap eCommerce Template">
     <meta name="author" content="p-themes">
--- index.php
+++ index.php
@@ -39,7 +39,7 @@ $categories = $stmt1->fetchAll(PDO::FETCH_ASSOC);
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <title>Molla - Bootstrap eCommerce Template</title>
+    <title>Molla eCommerce</title>
     <meta name="keywords" content="HTML5 Template" />
     <meta name="description" content="Molla - Bootstrap eCommerce Template" />
     <meta name="author" content="p-themes" />
@@ -492,58 +492,80 @@ $categories = $stmt1->fetchAll(PDO::FETCH_ASSOC);
                     <!-- End .mb-6 -->
 
 
-                    <div class="container">
-                        <hr class="mb-0" />
-                        <div class="owl-carousel mt-5 mb-5 owl-simple" data-toggle="owl" data-owl-options='{
-                        "nav": false, 
-                        "dots": false,
-                        "margin": 30,
-                        "loop": false,
-                        "responsive": {
-                            "0": {
-                                "items":2
-                            },
-                            "420": {
-                                "items":3
-                            },
-                            "600": {
-                                "items":4
-                            },
-                            "900": {
-                                "items":5
-                            },
-                            "1024": {
-                                "items":6
-                            }
-                        }
-                    }'>
-                            <a href="#" class="brand">
-                                <img src="assets/images/brands/1.png" alt="Brand Name" />
-                            </a>
+                    <div class="icon-boxes-container bg-transparent">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-sm-6 col-lg-3">
+                                    <div class="icon-box icon-box-side">
+                                        <span class="icon-box-icon text-dark">
+                                            <i class="icon-rocket"></i>
+                                        </span>
+                                        <div class="icon-box-content">
+                                            <h3 class="icon-box-title">Free Shipping</h3>
+                                            <!-- End .icon-box-title -->
+                                            <p>Orders 50 JD or more</p>
+                                        </div>
+                                        <!-- End .icon-box-content -->
+                                    </div>
+                                    <!-- End .icon-box -->
+                                </div>
+                                <!-- End .col-sm-6 col-lg-3 -->
 
-                            <a href="#" class="brand">
-                                <img src="assets/images/brands/2.png" alt="Brand Name" />
-                            </a>
+                                <div class="col-sm-6 col-lg-3">
+                                    <div class="icon-box icon-box-side">
+                                        <span class="icon-box-icon text-dark">
+                                            <i class="icon-rotate-left"></i>
+                                        </span>
 
-                            <a href="#" class="brand">
-                                <img src="assets/images/brands/3.png" alt="Brand Name" />
-                            </a>
+                                        <div class="icon-box-content">
+                                            <h3 class="icon-box-title">Free Returns</h3>
+                                            <!-- End .icon-box-title -->
+                                            <p>Within 30 days</p>
+                                        </div>
+                                        <!-- End .icon-box-content -->
+                                    </div>
+                                    <!-- End .icon-box -->
+                                </div>
+                                <!-- End .col-sm-6 col-lg-3 -->
 
-                            <a href="#" class="brand">
-                                <img src="assets/images/brands/4.png" alt="Brand Name" />
-                            </a>
+                                <div class="col-sm-6 col-lg-3">
+                                    <div class="icon-box icon-box-side">
+                                        <span class="icon-box-icon text-dark">
+                                            <i class="icon-info-circle"></i>
+                                        </span>
 
-                            <a href="#" class="brand">
-                                <img src="assets/images/brands/5.png" alt="Brand Name" />
-                            </a>
+                                        <div class="icon-box-content">
+                                            <h3 class="icon-box-title">Get 20% Off 1 Item</h3>
+                                            <!-- End .icon-box-title -->
+                                            <p>when you sign up</p>
+                                        </div>
+                                        <!-- End .icon-box-content -->
+                                    </div>
+                                    <!-- End .icon-box -->
+                                </div>
+                                <!-- End .col-sm-6 col-lg-3 -->
 
-                            <a href="#" class="brand">
-                                <img src="assets/images/brands/6.png" alt="Brand Name" />
-                            </a>
+                                <div class="col-sm-6 col-lg-3">
+                                    <div class="icon-box icon-box-side">
+                                        <span class="icon-box-icon text-dark">
+                                            <i class="icon-life-ring"></i>
+                                        </span>
+
+                                        <div class="icon-box-content">
+                                            <h3 class="icon-box-title">We Support</h3>
+                                            <!-- End .icon-box-title -->
+                                            <p>24/7 amazing services</p>
+                                        </div>
+                                        <!-- End .icon-box-content -->
+                                    </div>
+                                    <!-- End .icon-box -->
+                                </div>
+                                <!-- End .col-sm-6 col-lg-3 -->
+                            </div>
+                            <!-- End .row -->
                         </div>
-                        <!-- End .owl-carousel -->
+                        <!-- End .container -->
                     </div>
-                    <!-- End .container -->
 
                     <div class="container new-arrivals">
                         <div class="heading heading-flex mb-3">
@@ -653,80 +675,7 @@ $categories = $stmt1->fetchAll(PDO::FETCH_ASSOC);
                             </div>
                             <!-- End .container -->
 
-                            <div class="icon-boxes-container bg-transparent">
-                                <div class="container">
-                                    <div class="row">
-                                        <div class="col-sm-6 col-lg-3">
-                                            <div class="icon-box icon-box-side">
-                                                <span class="icon-box-icon text-dark">
-                                                    <i class="icon-rocket"></i>
-                                                </span>
-                                                <div class="icon-box-content">
-                                                    <h3 class="icon-box-title">Free Shipping</h3>
-                                                    <!-- End .icon-box-title -->
-                                                    <p>Orders 50 JD or more</p>
-                                                </div>
-                                                <!-- End .icon-box-content -->
-                                            </div>
-                                            <!-- End .icon-box -->
-                                        </div>
-                                        <!-- End .col-sm-6 col-lg-3 -->
 
-                                        <div class="col-sm-6 col-lg-3">
-                                            <div class="icon-box icon-box-side">
-                                                <span class="icon-box-icon text-dark">
-                                                    <i class="icon-rotate-left"></i>
-                                                </span>
-
-                                                <div class="icon-box-content">
-                                                    <h3 class="icon-box-title">Free Returns</h3>
-                                                    <!-- End .icon-box-title -->
-                                                    <p>Within 30 days</p>
-                                                </div>
-                                                <!-- End .icon-box-content -->
-                                            </div>
-                                            <!-- End .icon-box -->
-                                        </div>
-                                        <!-- End .col-sm-6 col-lg-3 -->
-
-                                        <div class="col-sm-6 col-lg-3">
-                                            <div class="icon-box icon-box-side">
-                                                <span class="icon-box-icon text-dark">
-                                                    <i class="icon-info-circle"></i>
-                                                </span>
-
-                                                <div class="icon-box-content">
-                                                    <h3 class="icon-box-title">Get 20% Off 1 Item</h3>
-                                                    <!-- End .icon-box-title -->
-                                                    <p>when you sign up</p>
-                                                </div>
-                                                <!-- End .icon-box-content -->
-                                            </div>
-                                            <!-- End .icon-box -->
-                                        </div>
-                                        <!-- End .col-sm-6 col-lg-3 -->
-
-                                        <div class="col-sm-6 col-lg-3">
-                                            <div class="icon-box icon-box-side">
-                                                <span class="icon-box-icon text-dark">
-                                                    <i class="icon-life-ring"></i>
-                                                </span>
-
-                                                <div class="icon-box-content">
-                                                    <h3 class="icon-box-title">We Support</h3>
-                                                    <!-- End .icon-box-title -->
-                                                    <p>24/7 amazing services</p>
-                                                </div>
-                                                <!-- End .icon-box-content -->
-                                            </div>
-                                            <!-- End .icon-box -->
-                                        </div>
-                                        <!-- End .col-sm-6 col-lg-3 -->
-                                    </div>
-                                    <!-- End .row -->
-                                </div>
-                                <!-- End .container -->
-                            </div>
                             <!-- End .icon-boxes-container -->
         </main>
         <!-- End .main -->
